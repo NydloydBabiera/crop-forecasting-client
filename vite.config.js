43f0b1abf67f6c5d55f3d0a73cc1ef8b0d160114
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2025-10-04 22:16:10 +0800

    port and host implementation into env

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 6001,
-  },
+export default defineConfig(({ mode }) => {
+  // Load .env files based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST,     // default host if not defined
+      port: Number(env.VITE_PORT),  // default port if not defined
+    },
+  }
 })
