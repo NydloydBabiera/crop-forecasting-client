290223b3cf141e869bbd9847101d75affeae7819
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2025-10-13 01:24:12 +0800

    added socket io connection

--- src/components/SensorValue.jsx
+++ src/components/SensorValue.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
 
 const SensorValue = () => {
+  const [sensorData, setSensorData] = useState({
+    temperature: 0,
+    humidity: 0,
+    soil_moisture: 0,
+    npk: 0,
+  });
+  const socketRef = useRef(null);
+  const apiUrl = import.meta.env.VITE_API_URL;
+  
+  useEffect(() => {
+    if (!socketRef.current) {
+      socketRef.current = io(apiUrl, {
+        transports: ["websocket"],
+        reconnectionAttempts: 5,
+        reconnectionDelay: 1000,
+      });
+
+      socketRef.current.on("connect", () => {
+        console.log("Socket connected:", socketRef.current.id);
+      });
+
+      socketRef.current.on("sensor-update", (data) => {
+        console.log("Received data:", data);
+        setSensorData(data);
+      });
+    }
+
+    // Cleanup
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
+    };
+  }, [apiUrl]);
+
   const cards = [
-    { title: "Temperature", value: "28°C", color: "bg-blue-500" },
-    { title: "Humidity", value: "74%", color: "bg-green-500" },
-    { title: "Soil moisture", value: "76%", color: "bg-purple-500" },
-    { title: "NPK", value: "85.24 mg/kg", color: "bg-red-500" },
-  ]
+    {
+      title: "Temperature",
+      value: `${sensorData.temperature}°C`,
+      color: "bg-blue-500",
+    },
+    {
+      title: "Humidity",
+      value: `${sensorData.humidity}%`,
+      color: "bg-green-500",
+    },
+    // {
+    //   title: "Soil Moisture",
+    //   value: `${sensorData.soil_moisture}%`,
+    //   color: "bg-purple-500",
+    // },
+    // { title: "NPK", value: `${sensorData.npk} mg/kg`, color: "bg-red-500" },
+  ];
+
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Crop Name</h1>
@@ -22,7 +72,7 @@ const SensorValue = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SensorValue
+export default SensorValue;
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // Load .env files based on the current mode (development, production, etc.)
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
