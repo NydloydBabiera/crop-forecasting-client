8c79437855216a5f3744c1f1b7ac1592334efdf0
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2025-11-03 06:52:57 +0800

    update express deployment

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const DIST_PATH = path.join(__dirname, 'dist');
+const DIST_PATH = path.join(__dirname, "dist");
+
+dotenv.config({ path: path.resolve(__dirname, '.env.local') });
 
 // Serve static files
 app.use(express.static(DIST_PATH));
 
 // Catch-all route for SPA (all other paths serve index.html)
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(DIST_PATH, 'index.html'));
+app.get("/*splat", async (req, res) => {
+  res.sendFile(path.join(DIST_PATH, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.VITE_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
