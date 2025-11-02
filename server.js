b7a465e16b3f107dbd6d2377a5cc21cf92aedb93
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2025-11-03 06:08:44 +0800

    update server.js

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ const DIST_PATH = path.join(__dirname, 'dist');
 // Serve static files
 app.use(express.static(DIST_PATH));
 
-// Catch-all route for SPA
-app.get('*', (req, res) => {
+// Catch-all route for SPA (all other paths serve index.html)
+app.get('/*', (req, res) => {
   res.sendFile(path.join(DIST_PATH, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
